e1d82c7ca576757b07f14bce27f3a9c0f68ff1c6
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-18 19:34:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from vars import var
 
 logging.getLogger("pyrogram").setLevel(logging.INFO)
 
-AnonyBot = Client(
-    "Anonymous-Sender",
+MhubBot = Client(
+    "Memehub_Bot",
     api_id=var.API_ID,
     api_hash=var.API_HASH,
     bot_token=var.BOT_TOKEN,
     plugins=dict(root="plugins"),
 )
 
-AnonyBot.start()
-uname = (AnonyBot.get_me()).username
+MhubBot.start()
+uname = (MhubBot.get_me()).username
 print(f"@{uname} Deployed Successfully !")
 
 idle()
