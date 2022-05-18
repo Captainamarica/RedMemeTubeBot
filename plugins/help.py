1e827bc389524fd817b29fc89e0cfeca9e10485f
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-18 20:35:38 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -57,6 +57,22 @@ PM_MED_ATT = "<b>Photo from:</b> {} \n<b>Name:</b> {}"
 
 @Client.on_message(filters.command(["help", "help@MemeHubTgSl_Bot"]))
 async def startprivate(bot, message):
+    if force_subchannel:
+        try:
+            user = await bot.get_chat_member(force_subchannel, message.from_user.id)
+            if user.status == "kicked out":
+                await message.reply_text("Yourt Banned")
+                return 
+        except UserNotParticipant:
+            file_id = "CAADBQADOAcAAn_zKVSDCLfrLpxnhAI"
+            await bot.send_sticker(message.chat.id, file_id)
+            text = FORCESUB_TEXT
+            reply_markup = FORCESUB_BUTTONS
+            await message.reply_text(
+            text=text,
+            reply_markup=reply_markup
+            ) 
+            return
     await message.reply_text(
         text=HELP_STRING,
         reply_markup=CLOSE_BUTTON,
