4deea450ddc6bfb2643a870d983065f9d18797dd
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-05-20 08:21:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -162,7 +162,38 @@ async def replay_media(bot, message):
             chat_id=int(reference_id),
             from_chat_id=message.chat.id,
             message_id=message.message_id
-        )          
+        )  
+        
+        
+@Client.on_callback_query()  
+async def tgm(bot, update):  
+    if update.data == "add": 
+        await update.answer(
+             text="♻️Adding Soon.....",
+        )
+    elif update.data == "bak":
+         await update.message.edit_text(
+             text=HELP_STRING,
+             reply_markup=CLOSE_BUTTON,
+             disable_web_page_preview=True
+         )
+         await update.answer(
+             text="👻 ʙᴀᴍᴄᴋ 👻",
+         )
+    elif update.data == "bak":
+         await update.message.delete()
+         await bot.delete_message(update.chat.id, update.message.id)
+    elif update.data == "hlp":
+         await update.message.edit_text(
+             text=HELP_STRING,
+             reply_markup=CLOSE_BUTTON,
+             disable_web_page_preview=True
+         )
+         await update.answer(
+             text="👻 ʜᴇᴍʟᴘ 👻",
+         )
+    elif update.data == "cloc":
+         await update.message.delete()
          
            
 
